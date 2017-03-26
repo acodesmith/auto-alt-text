65c6af5f11c4add306480ce47b9bb71cf656a38d
Steven Slack <steven@alleyinteractive.com>
2017-03-26 16:46:53 -0400

    basic formatting first round

--- auto-alt-text.php
+++ auto-alt-text.php
@@ -1,6 +1,6 @@
-
 <?php
-/*
+/**
+ *
  * Plugin Name: Auto Alt Text
  * Version: 0.0.1
  * Plugin URI: http://www.acodesmith.com
@@ -10,11 +10,11 @@
  * Requires at least: 4.0
  * Tested up to: 4.7
  *
- *
  * @package WordPress
  * @author Adam Smith
  * @since 0.0.1
- */
+ **/
+
 define( 'AAT_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'AAT_PLUGIN_URI', plugin_dir_url( __FILE__ ) );
 
@@ -22,86 +22,87 @@ define( 'AAT_PLUGIN_URI', plugin_dir_url( __FILE__ ) );
  * Load all the custom classes
  */
 function auto_alt_text_init() {
-    auto_alt_text_load();
-    add_filter('add_attachment', 'auto_alt_text_add_attachment_hook');
+	auto_alt_text_load();
+	add_filter( 'add_attachment', 'auto_alt_text_add_attachment_hook' );
 }
 add_action( 'init', 'auto_alt_text_init' );
 
 /**
  * Add Auto Alt Text submenu item to the settings admin section
  */
-function auto_alt_text_setup()
-{
-    add_submenu_page( 'options-general.php', 'Auto Alt Text', 'Auto Alt Text', 'manage_options', 'auto-alt-text', 'auto_alt_text_admin_page' );
+function auto_alt_text_setup() {
+	add_submenu_page(
+		'options-general.php',
+		__( 'Auto Alt Text', 'aat' ),
+		__( 'Auto Alt Text', 'aat' ),
+		'manage_options',
+		'auto-alt-text',
+		'auto_alt_text_admin_page'
+	);
 }
 add_action( 'admin_menu', 'auto_alt_text_setup' );
 
 /**
  * Admin Page for updating all Auto Alt Text settings
  */
-function auto_alt_text_admin_page()
-{
-    Auto_Alt_Text_Admin::scripts();
+function auto_alt_text_admin_page() {
+	Auto_Alt_Text_Admin::scripts();
 
-    if( ! empty( $_POST ) ) {
+	if ( ! empty( $_POST ) ) {
+		Auto_Alt_Text_Admin::processPost();
+	}
 
-        Auto_Alt_Text_Admin::processPost();
-    }
+	/** Variables for the view */
+	$nonce_action    = Auto_Alt_Text_Common::NONCE_NAMESPACE;
+	$confidence      = Auto_Alt_Text_Common::getConfidence();
+	$prefix          = Auto_Alt_Text_Common::getAltPrefix();
+	$hasBatched      = Auto_Alt_Text_Admin::hasRanBatchAtLeastOnce();
+	$selectedService = Auto_Alt_Text_Common::getSelectedService();
+	$hasAuth         = false;
 
-    /** Variables for the view */
-    $nonce_action       = Auto_Alt_Text_Common::NONCE_NAMESPACE;
-    $confidence         = Auto_Alt_Text_Common::getConfidence();
-    $prefix             = Auto_Alt_Text_Common::getAltPrefix();
-    $hasBatched         = Auto_Alt_Text_Admin::hasRanBatchAtLeastOnce();
-    $selectedService    = Auto_Alt_Text_Common::getSelectedService();
-    $hasAuth            = false;
+	Alt_Text_Service_Switch::$service = $selectedService;
 
-    Alt_Text_Service_Switch::$service = $selectedService;
+	/** @var Auto_Alt_Text_Aws $service */
+	if ( $service = Alt_Text_Service_Switch::instance() ) {
 
-    /** @var Auto_Alt_Text_Aws $service */
-    if( $service = Alt_Text_Service_Switch::instance() ) {
+		$hasAuth = ! empty( $service->auth() );
+	}
 
-        $hasAuth = ! empty( $service->auth() );
-    }
-
-    include( __DIR__ .'/views/batch.php' );
-    include( __DIR__ . '/views/admin.php' );
+	include( __DIR__ .'/views/batch.php' );
+	include( __DIR__ . '/views/admin.php' );
 }
 
 /**
  * @param array $groups
  */
-function auto_alt_text_load()
-{
-    //Classes
-    require( __DIR__ . '/classes/auto-alt-text-service-interface.php');
-    require( __DIR__ . '/classes/auto-alt-text-service-switch.php');
-    require( __DIR__ . '/classes/auto-alt-text-admin.php');
-    require( __DIR__ . '/classes/auto-alt-text-batch.php' );
-    require( __DIR__ . '/classes/auto-alt-text-admin-batch.php');
-    require( __DIR__ . '/classes/auto-alt-text-common.php');
-    require( __DIR__ . '/classes/auto-alt-text-db.php' );
+function auto_alt_text_load() {
+	// Classes
+	require( __DIR__ . '/classes/auto-alt-text-service-interface.php');
+	require( __DIR__ . '/classes/auto-alt-text-service-switch.php');
+	require( __DIR__ . '/classes/auto-alt-text-admin.php');
+	require( __DIR__ . '/classes/auto-alt-text-batch.php' );
+	require( __DIR__ . '/classes/auto-alt-text-admin-batch.php');
+	require( __DIR__ . '/classes/auto-alt-text-common.php');
+	require( __DIR__ . '/classes/auto-alt-text-db.php' );
 
-    //Functions
-    require( __DIR__ . '/functions/auto-alt-text-add-attachment.php' );
+	// Functions
+	require( __DIR__ . '/functions/auto-alt-text-add-attachment.php' );
 }
 
 /**
  * Stages based on the admin batch process.
  */
-function auto_alt_text_batch_button()
-{
-    switch( $_GET['stage'] )
-    {
-        case 'start':
-            Auto_Alt_Text_Admin_Batch::start();
-            break;
-        case 'processing':
-            Auto_Alt_Text_Admin_Batch::batch();
-            break;
-    }
+function auto_alt_text_batch_button() {
+	switch( $_GET['stage'] ) {
+		case 'start':
+			Auto_Alt_Text_Admin_Batch::start();
+			break;
+		case 'processing':
+			Auto_Alt_Text_Admin_Batch::batch();
+			break;
+	}
 
-    die;
+	die;
 }
 add_action( 'wp_ajax_aat_batch', 'auto_alt_text_batch_button' );
 
