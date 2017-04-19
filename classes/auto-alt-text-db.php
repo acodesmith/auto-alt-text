e6d10db67af7529373c7077b8ebf5d3c35edc00c
Steven Slack <steven@alleyinteractive.com>
2017-04-19 11:41:11 -0400

    cache queries based on key and set to expire in an hour

--- classes/auto-alt-text-db.php
+++ classes/auto-alt-text-db.php
@@ -6,6 +6,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class Auto_Alt_Text_Db {
+
+	/**
+	 * Get transient key
+	 *
+	 * @return string
+	 */
+	private static function get_transient_key( $key ) {
+		return 'aat_' . md5( join( '.', func_get_args() ) );
+	}
+
 	/**
 	 * Returns either the total number of images needing processing or
 	 * all the images without alt tags in array of objects.
@@ -21,15 +31,26 @@ class Auto_Alt_Text_Db {
 		$select      = $count ? "SELECT COUNT(ID) as total FROM {$wpdb->posts}" : "SELECT * FROM {$wpdb->posts} ";
 		$limit_query = false !== $limit ? 'LIMIT ' . ( $offset > 0 ? "{$limit}, {$offset}" : "{$limit}" ) : '';
 
-		return $wpdb->get_results( "
-			{$select} WHERE ID NOT IN (
-			SELECT ID from {$wpdb->posts}
-			JOIN {$wpdb->postmeta} on {$wpdb->posts}.ID = {$wpdb->postmeta}.post_id
-			WHERE $wpdb->postmeta.meta_key = '_wp_attachment_image_alt'
-			AND {$wpdb->posts}.post_type = 'attachment'
-			) AND {$wpdb->posts}.post_type = 'attachment'
-			{$limit_query}
-		" );
+		$cache_key = self::get_transient_key( $limit, $offset, $count );
+		$query = wp_cache_get( $cache_key );
+
+		if ( false === $query ) {
+
+			$query = $wpdb->get_results( "
+				{$select} WHERE ID NOT IN (
+				SELECT ID from {$wpdb->posts}
+				JOIN {$wpdb->postmeta} on {$wpdb->posts}.ID = {$wpdb->postmeta}.post_id
+				WHERE $wpdb->postmeta.meta_key = '_wp_attachment_image_alt'
+				AND {$wpdb->posts}.post_type = 'attachment'
+				) AND {$wpdb->posts}.post_type = 'attachment'
+				{$limit_query}
+			" );
+
+			// set our cache to expire in 1 hour
+			wp_cache_set( $cache_key, $query, 60 * MINUTES_IN_SECONDS );
+		}
+
+		return $query;
 	}
 
 	/**
@@ -42,15 +63,26 @@ class Auto_Alt_Text_Db {
 
 		$limit_query = ( false !== $limit ) ? "LIMIT {$limit}" : '';
 
-		return $wpdb->get_results( "
-			SELECT * FROM {$wpdb->posts} WHERE ID NOT IN (
-			SELECT ID from {$wpdb->posts}
-			JOIN {$wpdb->postmeta} on {$wpdb->posts}.ID = {$wpdb->postmeta}.post_id
-			WHERE {$wpdb->postmeta}.meta_key = '_wp_attachment_image_alt'
-			AND {$wpdb->posts}.post_type = 'attachment'
-			) AND {$wpdb->posts}.post_type = 'attachment' AND {$wpdb->posts}.post_parent = {$post_id}
-			{$limit_query}
-		" );
+		$cache_key = self::get_transient_key( $post_id, $limit );
+		$query = wp_cache_get( $cache_key );
+
+		if ( false === $query ) {
+
+			return $wpdb->get_results( "
+				SELECT * FROM {$wpdb->posts} WHERE ID NOT IN (
+				SELECT ID from {$wpdb->posts}
+				JOIN {$wpdb->postmeta} on {$wpdb->posts}.ID = {$wpdb->postmeta}.post_id
+				WHERE {$wpdb->postmeta}.meta_key = '_wp_attachment_image_alt'
+				AND {$wpdb->posts}.post_type = 'attachment'
+				) AND {$wpdb->posts}.post_type = 'attachment' AND {$wpdb->posts}.post_parent = {$post_id}
+				{$limit_query}
+			" );
+
+			// set our cache to expire in 1 hour
+			wp_cache_set( $cache_key, $query, 60 * MINUTES_IN_SECONDS );
+		}
+
+		return $query;
 	}
 
 	/**
@@ -63,14 +95,24 @@ class Auto_Alt_Text_Db {
 
 		$limit_query = false !== $limit ? "LIMIT $limit" : '';
 
-		return $wpdb->get_results( "
-			SELECT * FROM {$wpdb->posts} WHERE ID NOT IN (
-			SELECT ID from {$wpdb->posts}
-			JOIN {$wpdb->postmeta} on {$wpdb->posts}.ID = {$wpdb->postmeta}.post_id
-			WHERE {$wpdb->postmeta}.meta_key = '_wp_attachment_image_alt'
-			AND {$wpdb->posts}.post_type = 'attachment'
-			) AND {$wpdb->posts}.post_type = 'attachment' AND {$wpdb->posts}.ID = {$attachment_id}
-			{$limit_query}
-		" );
+		$cache_key = self::get_transient_key( $attachment_id, $limit );
+		$query = wp_cache_get( $cache_key );
+
+		if ( false === $query ) {
+			return $wpdb->get_results( "
+				SELECT * FROM {$wpdb->posts} WHERE ID NOT IN (
+				SELECT ID from {$wpdb->posts}
+				JOIN {$wpdb->postmeta} on {$wpdb->posts}.ID = {$wpdb->postmeta}.post_id
+				WHERE {$wpdb->postmeta}.meta_key = '_wp_attachment_image_alt'
+				AND {$wpdb->posts}.post_type = 'attachment'
+				) AND {$wpdb->posts}.post_type = 'attachment' AND {$wpdb->posts}.ID = {$attachment_id}
+				{$limit_query}
+			" );
+
+			// set our cache to expire in 1 hour
+			wp_cache_set( $cache_key, $query, 60 * MINUTES_IN_SECONDS );
+		}
+
+		return $query;
 	}
 }
